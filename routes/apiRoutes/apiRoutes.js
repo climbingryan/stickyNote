1a65de983283b2e7dd53ae5f1b969471b9a0b918
shaggy <simmons5family@gmail.com>
2020-12-12 20:53:19 -0600

    wrote api Routes get & post, need to code functions called in api routes

---
+++ routes/apiRoutes/apiRoutes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const { verifyNotes, createNotes } = require('../../lib/notes');
+const { notes } = require('../../data/noteObj');
+
+    // return current notes obj
+router.get('/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+})
+
+router.post('./notes', (req, res) => {
+
+    req.body.id = notes.length.toString();
+
+    if(!verifyNotes(req.body)) {
+        res.status(400).send('Please format your note correctly :)');
+    } else {
+        const note = createNotes(req.body, notes);
+        res.json(note);
+    }
+})
+
+module.exports = router;
