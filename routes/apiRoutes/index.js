1d100fef18d4ab274121fce08a5d8634ecc05565
shaggy <simmons5family@gmail.com>
2020-12-13 20:04:30 -0600

    server changes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,12 +3,12 @@ const { verifyNotes, createNotes } = require('../../lib/routeFunctions');
 const { notes } = require('../../data/db.json');
 
     // return current notes obj
-router.get('/notes', (req, res) => {
+router.get("/notes", (req, res) => {
     let results = notes;
     res.json(results);
 })
 
-router.post('./notes', (req, res) => {
+router.post("/notes/", (req, res) => {
 
     req.body.id = notes.length.toString();
 
--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const PORT = process.env.PORT || 3001;
 const apiRoute = require('./routes/apiRoutes/index');
 const htmlRoute = require('./routes/htmlRoutes/index');
 
-    // allows access to css, images & js files w/o creating specific routes
-app.use(express.static('public'));
 
     // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
     // parse incoming JSON data
 app.use(express.json());
- 
     // Html and Api routes
 app.use('/', htmlRoute);
 app.use('/api', apiRoute);
+    // allows access to css, images & js files w/o creating specific routes
+app.use(express.static('public'));
+
+
 
 
 app.listen(PORT, () => {
